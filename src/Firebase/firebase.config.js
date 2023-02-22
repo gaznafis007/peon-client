581d14470d3dbb795f6297c643439c021a4be00c
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-02-22 11:45:57 +0600

    basic structure built

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQR35AgKPNBoTJjd8e6KxoclLTFslCooM",
+  authDomain: "peon-client.firebaseapp.com",
+  projectId: "peon-client",
+  storageBucket: "peon-client.appspot.com",
+  messagingSenderId: "919658398362",
+  appId: "1:919658398362:web:b7e9c44c1e2157014e9523",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,14 +1,52 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
   const AuthContext = createContext();
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(true);
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const getProfile = (displayName, imgUrl) => {
+    return updateProfile(auth.currentUser, {
+      displayName: displayName,
+      photoURL: imgUrl,
+    });
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
   const value = {
     user,
     loading,
     setLoading,
+    signUp,
+    signIn,
+    getProfile,
+    logOut,
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
